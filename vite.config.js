38d9788f4166601fde6d136696930c66ba509850
Olgu Uysal <olguuysal@192.168.1.102>
2026-01-12 01:04:50 +0300

    feat: add careers page and update footer links

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
                 api: resolve(__dirname, 'api.html'),
                 insights: resolve(__dirname, 'insights.html'),
                 post: resolve(__dirname, 'post.html'),
+                careers: resolve(__dirname, 'careers.html'),
                 '404': resolve(__dirname, '404.html'),
             }
         }
